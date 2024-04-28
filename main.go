8f79f416a95ba7b50128b8196707049a0ec760eb
Andy Kreling <krelinga@gmail.com>
2024-04-28 19:50:02 +0000

    very basic main().

--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
-//import (
-//    "github.com/krelinga/mkv-util-server/pb"
-//)
+import (
+    "log"
+    "net"
+
+    "github.com/krelinga/mkv-util-server/pb"
+    "google.golang.org/grpc"
+)
+
+type MkvUtilsServer struct {
+    pb.UnimplementedMkvUtilsServer
+}
+
+func MainOrError() error {
+    lis, err := net.Listen("tcp", ":25002")
+    if err != nil {
+        return err
+    }
+    grpcServer := grpc.NewServer()
+    pb.RegisterMkvUtilsServer(grpcServer, &MkvUtilsServer{})
+    grpcServer.Serve(lis)  // Runs as long as the server is alive.
+
+    return nil
+}
 
 func main() {
-    
+    if err := MainOrError(); err != nil {
+        log.Fatal(err)
+    }
 }
