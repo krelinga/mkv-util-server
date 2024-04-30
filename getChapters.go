ee2363ebacb9e82d90dfa972a3c176ee476ebfdb
Andy Kreling <krelinga@gmail.com>
2024-04-30 22:12:56 +0000

    routing boilerplate for GetChapters().

--- getChapters.go
+++ getChapters.go
@@ -3,10 +3,30 @@ package main
 import (
     "context"
     "errors"
+    "fmt"
 
     "github.com/krelinga/mkv-util-server/pb"
 )
 
-func getChapters(ctx context.Context, r *pb.GetChaptersRequest) (*pb.GetChaptersReply, error) {
+func getChaptersSimple(ctx context.Context, path string) (*pb.SimpleChapters, error) {
     return nil, errors.New("Not implemented")
+}
+
+func getChapters(ctx context.Context, r *pb.GetChaptersRequest) (*pb.GetChaptersReply, error) {
+    resp := &pb.GetChaptersReply {}
+    switch r.Format {
+    case pb.ChaptersFormat_CF_SIMPLE:
+        simple, err := getChaptersSimple(ctx, r.InPath)
+        if err != nil {
+            return nil, err
+        }
+        resp.Chapters = &pb.Chapters{
+            Format: pb.ChaptersFormat_CF_SIMPLE,
+            Simple: simple,
+        }
+    default:
+        return nil, fmt.Errorf("Unsupported format: %v", r.Format)
+    }
+
+    return resp, nil
 }
