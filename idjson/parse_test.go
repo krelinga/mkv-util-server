83029a2f6fc70f6a9858a1eb5cf5dc9c3d5fa1f1
Andy Kreling <krelinga@gmail.com>
2024-05-03 21:48:56 +0000

    extend testing for Parse() to pull in actual data.

--- idjson/parse_test.go
+++ idjson/parse_test.go
@@ -1,7 +1,30 @@
 package idjson
 
-import "testing"
+import (
+    "io"
+    "os"
+    "path/filepath"
+    "runtime"
+    "testing"
+)
+
+func unsafeOpenTestData() io.ReadCloser {
+    _, thisPath, _, _ := runtime.Caller(0)
+    p := filepath.Join(filepath.Dir(thisPath), "../testdata/id.json")
+    f, err := os.Open(p)
+    if err != nil {
+        panic(err)
+    }
+    return f
+}
 
 func TestParse(t *testing.T) {
     t.Parallel()
+    f := unsafeOpenTestData()
+    defer f.Close()
+
+    _, err := Parse(f)
+    if err != nil {
+        t.Error(err)
+    }
 }
