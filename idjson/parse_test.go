3152ba6aa6600f75fe01ef55a3a5934c6d41c2c5
Andy Kreling <krelinga@gmail.com>
2024-05-03 21:53:15 +0000

    test json parsing.

--- idjson/parse_test.go
+++ idjson/parse_test.go
@@ -6,6 +6,8 @@ import (
     "path/filepath"
     "runtime"
     "testing"
+
+    "github.com/google/go-cmp/cmp"
 )
 
 func unsafeOpenTestData() io.ReadCloser {
@@ -23,8 +25,21 @@ func TestParse(t *testing.T) {
     f := unsafeOpenTestData()
     defer f.Close()
 
-    _, err := Parse(f)
+    out, err := Parse(f)
     if err != nil {
         t.Error(err)
+    }
+    expected := &MkvMerge{
+        Tracks: []*Track{
+            {
+                Type: "video",
+                Properties: &TrackProperties{
+                    TagDuration: "00:00:13.346000000",
+                },
+            },
+        },
+    }
+    if !cmp.Equal(expected, out) {
+        t.Error(cmp.Diff(expected, out))
     }
 }
