228d6431c3d0b07163814ca0ebe099f27107ffeb
Andy Kreling <krelinga@gmail.com>
2024-05-03 01:38:54 +0000

    use regex to match the various parts of a chapter file.

--- simpleChapters.go
+++ simpleChapters.go
@@ -4,28 +4,35 @@ import (
     "bufio"
     "fmt"
     "io"
-    //"regexp"
-    //"strings"
+    "regexp"
+    "strings"
 
     "github.com/krelinga/mkv-util-server/pb"
 )
 
-//var (
-//    simpleChapterRe = regexp.MustCompile(`^CHAPTER(\d+)=(\d)+:(\d+):(\d+)\.(\d+)
-//CHAPTER(\d+)NAME=(.*+)`)
-//)
+var (
+    simpleChapterRe = regexp.MustCompile(`^CHAPTER(\d+)=(\d)+:(\d+):(\d+)\.(\d+)
+CHAPTER(\d+)NAME=(.+)`)
+)
 
 func parseSimpleChapters(r io.Reader) (*pb.SimpleChapters, error) {
     scanner := bufio.NewScanner(r)
     first := true
-    //var part string
+    var part string
     chapters := &pb.SimpleChapters{}
     for scanner.Scan() {
         if first {
-            // part = scanner.Text()
+            part = scanner.Text()
         } else {
-            // part = strings.Join([]string{part, scanner.Text()
+            part = strings.Join([]string{part, scanner.Text()}, "\n")
+            matches := simpleChapterRe.FindStringSubmatch(part)
+            for i, match := range matches {
+                if len(match) == 0 {
+                    return nil, fmt.Errorf("Could not match part %d", i)
+                }
+            }
             chapters.Chapters = append(chapters.Chapters, &pb.SimpleChapters_Chapter{})
+            part = ""
         }
         first = !first
     }
